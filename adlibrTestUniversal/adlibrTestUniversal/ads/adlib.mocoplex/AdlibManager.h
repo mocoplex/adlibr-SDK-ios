90ac7f9d438421e41e357af6e8cf8cffa665bd9f
berrybana <hana@mocoplex.com>
2013-03-18 19:03:03 +0900

    mod license info

--- adlibrTestUniversal/adlibrTestUniversal/ads/adlib.mocoplex/AdlibManager.h
+++ adlibrTestUniversal/adlibrTestUniversal/ads/adlib.mocoplex/AdlibManager.h
@@ -1,3 +1,10 @@
+/*
+ * adlibr - Library for mobile AD mediation.
+ * http://adlibr.com
+ * Copyright (c) 2012-2013 Mocoplex, Inc.  All rights reserved.
+ * Licensed under the BSD open source license.
+ */
+
 #import "SubAdlibAdViewCore.h"
 
 @protocol AdlibManagerDelegate <NSObject>
