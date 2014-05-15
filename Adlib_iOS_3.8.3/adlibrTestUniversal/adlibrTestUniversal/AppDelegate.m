f162fdf3af79c9045016da6e2493f73bc134abb4
berrybana <hana@mocoplex.com>
2014-05-15 12:26:24 +0900

    update

--- Adlib_iOS_3.8.3/adlibrTestUniversal/adlibrTestUniversal/AppDelegate.m
+++ Adlib_iOS_3.8.3/adlibrTestUniversal/adlibrTestUniversal/AppDelegate.m
@@ -52,8 +52,7 @@
     [[AdlibManager sharedSingletonClass] setPlatform:@"ADHUB" withClass:[SubAdlibAdViewAdHub class]];
     [[AdlibManager sharedSingletonClass] setPlatform:@"UPLUSAD" withClass:[SubAdlibAdViewUPlusAD class]];
     [[AdlibManager sharedSingletonClass] setPlatform:@"MMEDIA" withClass:[SubAdlibAdViewMMedia class]];
-    [MMSDK initialize];  // MillennialMedia를 사용하시려면 반드시 초기화를 호출해 주세요.
-    // 쓰지 않을 광고 플랫폼은 삭제해주세요.
+    [MMSDK initialize];  // MillennialMedia v5.2.0 이상을 사용하시려면 반드시 초기화를 호출해 주세요.
     
 }
 
