2ed0bb29ab5ab7e8a6b13b650a7599f46859d2f9
berrybana <hana@mocoplex.com>
2014-09-11 19:27:46 +0900

    update appdelegate.m

--- Adlib_iOS_3.8.4/adlibrTestUniversal/adlibrTestUniversal/AppDelegate.m
+++ Adlib_iOS_3.8.4/adlibrTestUniversal/adlibrTestUniversal/AppDelegate.m
@@ -22,8 +22,8 @@
 #import "SubAdlibAdViewAdHub.h"
 #import "SubAdlibAdViewUPlusAD.h"
 #import "SubAdlibAdViewMMedia.h"
-#import <MillennialMedia/MMSDK.h>
 #import "SubAdlibAdViewMedibaAd.h"
+#import <MillennialMedia/MMSDK.h>
 
 
 @implementation AppDelegate
@@ -52,8 +52,8 @@
     [[AdlibManager sharedSingletonClass] setPlatform:@"DOMOB" withClass:[SubAdlibAdViewDomob class]];
     [[AdlibManager sharedSingletonClass] setPlatform:@"ADHUB" withClass:[SubAdlibAdViewAdHub class]];
     [[AdlibManager sharedSingletonClass] setPlatform:@"UPLUSAD" withClass:[SubAdlibAdViewUPlusAD class]];
-    [[AdlibManager sharedSingletonClass] setPlatform:@"MMEDIA" withClass:[SubAdlibAdViewMMedia class]];
     [[AdlibManager sharedSingletonClass] setPlatform:@"MEDIBAAD" withClass:[SubAdlibAdViewMedibaAd class]];
+    [[AdlibManager sharedSingletonClass] setPlatform:@"MMEDIA" withClass:[SubAdlibAdViewMMedia class]];
     [MMSDK initialize];  // MillennialMedia v5.2.0 이상을 사용하시려면 반드시 초기화를 호출해 주세요.
     
 }
