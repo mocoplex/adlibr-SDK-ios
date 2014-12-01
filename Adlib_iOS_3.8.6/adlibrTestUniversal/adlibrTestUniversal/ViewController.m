4220e944cecea1c3aa80c51645a0d43fe5ad87ce
yongsunkim <yongsunkim@mocoplex.com>
2014-12-01 17:14:20 +0900

    update adlib SDK - bug fixed :  iOS 8 screen bounds in landscape orientation

---
+++ Adlib_iOS_3.8.6/adlibrTestUniversal/adlibrTestUniversal/ViewController.m
@@ -0,0 +1,225 @@
+/*
+ * adlibr - Library for mobile AD mediation.
+ * http://adlibr.com
+ * Copyright (c) 2012-2013 Mocoplex, Inc.  All rights reserved.
+ * Licensed under the BSD open source license.
+ */
+
+#import "ViewController.h"
+#import <Adlib/Adlib.h>
+
+@implementation ViewController
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [button addTarget:self action:@selector(btnClicked:) forControlEvents:UIControlEventTouchDown];
+    button.frame = CGRectMake(80, 210, 160, 40);
+    [button setTitle: @"Load InterstitialAd" forState: UIControlStateNormal];
+    
+    [self.view addSubview:button];
+    
+    // pop banner 설정 및 호출
+    // 프레임 컬러 설정(생략가능) : 기본 blackColor
+    [[AdlibManager sharedSingletonClass] setAdlibPopFrameColor:[UIColor darkGrayColor]];
+    // 버튼 컬러 설정(생략가능) : 기본 AdlibPopBtnStyleWhite
+    [[AdlibManager sharedSingletonClass] setAdlibPopCloseButtonStyle:AdlibPopBtnStyleWhite];
+    // 인, 아웃 애니메이션 설정(생략가능) : 기본 AdlibPopAnimationTypeSlide
+    [[AdlibManager sharedSingletonClass] setAdlibPopInAnimation:AdlibPopAnimationTypeSlide outAnimation:AdlibPopAnimationTypeNone];
+    // pop banner 보이기
+    [[AdlibManager sharedSingletonClass] showAdlibPopBanner:AdlibPopAlignBottom withPadding:60 withDelegate:self];
+    
+    // 서버에서 설정한 버전정보를 가져옵니다.
+    // 기존 클라이언트 버전을 확인하여 적절한 작업을 수행하세요.
+    NSString* ver = [[AdlibManager sharedSingletonClass] getCurrentVersion];
+    if( ver != nil)
+    {
+        double now = 1.0;
+        if(now < [ver doubleValue])
+        {
+            UIAlertView *alert = [[UIAlertView alloc] init];
+            [alert setTitle:@"버전확인"];
+            [alert setMessage:@"새로운 버전이 업데이트 되었습니다.\n지금 업데이트 하시겠습니까?"];
+            [alert setDelegate:nil];
+            [alert addButtonWithTitle:@"Yes"];  // 앱스토어 이동 등의 액션은 별도 구현해주세요.
+            [alert addButtonWithTitle:@"No"];
+            [alert show];
+            [alert release];            
+        }
+    }    
+}
+
+- (void)btnClicked:(id)sender
+{
+    // 전면광고 호출
+    [[AdlibManager sharedSingletonClass] loadInterstitialAd:self withDelegate:self];
+}
+
+//pop banner delegate:optional
+-(void)didReceiveAdlibPopAd
+{
+    NSLog(@"%@",@"Pop banner 수신 성공!!");
+}
+
+-(void)didFailToReceiveAdlibPopAd
+{
+    NSLog(@"%@",@"Pop banner 수신 실패..");
+}
+
+-(void)didCloseAdlibPopAd
+{
+    NSLog(@"%@",@"Pop banner 닫힘..");
+}
+
+//전면광고 관련 delegate:optional
+-(void)didReceiveAdlibInterstitialAd:(NSString*)from
+{
+    NSLog(@"%@ Interstitial Ad 수신 성공!!",from);
+}
+
+-(void)didFailToReceiveAdlibInterstitialAd:(NSString*)from
+{
+    NSLog(@"%@ Interstitial Ad 수신 실패..", from);
+}
+
+-(void)didCloseAdlibInterstitialAd:(NSString*)from
+{
+    NSLog(@"%@ Interstitial Ad 닫힘..", from);
+}
+
+-(void)didFailToReceiveAllInterstitialAd
+{
+    NSLog(@"%@", @"설정된 모든 Interstitial Ad 수신 실패");
+}
+
+// 광고를 수신했다. 광고view 의 크기와 위치를 재설정한다.
+- (void)layoutForAD
+{
+    CGPoint adPoint = CGPointMake(0, 0);
+    CGRect rt = self.view.frame;
+    
+    // 광고 view의 크기
+    CGSize sz = [[AdlibManager sharedSingletonClass] size];
+    
+    NSLog(@"layout : %@",NSStringFromCGSize(sz));
+    
+    BOOL bBannerBottom = YES;
+    
+    if(!bBannerBottom)
+    {
+        // 상단에 광고 위치
+        rt = CGRectMake(0, sz.height, rt.size.width, self.view.bounds.size.height-sz.height);
+    }
+    else
+    {
+        // 하단에 광고 위치        
+        rt = CGRectMake(0, 0, rt.size.width, self.view.bounds.size.height-sz.height);
+        adPoint = CGPointMake(0, self.view.bounds.size.height-sz.height);
+    }
+    
+    [[AdlibManager sharedSingletonClass] moveAdContainer:adPoint];
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [[AdlibManager sharedSingletonClass] attach:self withView:self.view withReceiver:@selector(layoutForAD)];
+    
+    // 또는 페이지 이름을 지정하여, 스케줄 설정에 이용할 수 있습니다.
+    //[[AdlibManager sharedSingletonClass] attach:self withView:self.view withDelegate:self withPageName:@"pagename"] ;
+    
+    
+    // 리워드링크 적용 예
+    // rewardLinkId는 애드립 홈페이지에서 발급받은 링크 ID로 대체하세요. 아래는 테스트키 입니다.
+    // addRewardLinkIcon을 추가하면 아이콘을 여러개 붙일 수 있습니다. 단 한 view에서 같은 키의 아이콘을 사용할 수 없습니다.
+    [[AdlibRewardLink sharedSingletonClass] attachRewardLink:self withView:self.view];
+    [[AdlibRewardLink sharedSingletonClass] addRewardLinkIcon:@"5279055ce4b0e87732589555" atPoint:CGPointMake(50, 50)];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+/*
+// delegate 로 연결한 경우
+- (void)gotAd
+{
+    CGPoint adPoint = CGPointMake(0, 0);
+    CGRect rt = self.view.frame;
+    
+    // 광고 view의 크기
+    CGSize sz = [[AdlibManager sharedSingletonClass] size];
+    
+    NSLog(@"layout : %@",NSStringFromCGSize(sz));
+    
+    BOOL bBannerBottom = YES;
+    
+    if(!bBannerBottom)
+    {
+        // 상단에 광고 위치
+        rt = CGRectMake(0, sz.height, rt.size.width, self.view.bounds.size.height-sz.height);
+    }
+    else
+    {
+        // 하단에 광고 위치
+        rt = CGRectMake(0, 0, rt.size.width, self.view.bounds.size.height-sz.height);
+        adPoint = CGPointMake(0, self.view.bounds.size.height-sz.height);
+    }
+    
+    [[AdlibManager sharedSingletonClass] moveAdContainer:adPoint];
+}
+
+-(void)didReceiveAdlibAd:(NSString*)from
+{
+    NSLog(@"%@ : Ad 수신 성공",from);
+}
+
+-(void)didFailToReceiveAdlibAd:(NSString*)from
+{
+    NSLog(@"%@ : Ad 수신 실패",from);
+}
+*/
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [[AdlibManager sharedSingletonClass] detach:self];
+    
+    // 리워드링크
+    [[AdlibRewardLink sharedSingletonClass] detachRewardLink:self];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+@end
---
+++ Adlib_iOS_3.8.6/adlibrTestUniversal/adlibrTestUniversal/ads/adlib.mocoplex/SubAdView/SubAdlibAdViewMMedia.m
@@ -0,0 +1,142 @@
+/*
+ * adlibr - Library for mobile AD mediation.
+ * http://adlibr.com
+ * Copyright (c) 2012-2013 Mocoplex, Inc.  All rights reserved.
+ * Licensed under the BSD open source license.
+ */
+
+/*
+ * confirmed compatible with MillennialMedia SDK 5.3.0
+ */
+
+#import "SubAdlibAdViewMMedia.h"
+
+/*
+ MillennialMedia v5.2.0 이상부터는
+ AppDelegate의 didFinishLaunchingWithOptions 메소드에서
+ [MMSDK initialize];
+ 를 반드시 호출해 주세요.
+ */
+
+// MILLENNIAL MEDIA의 APP 아이디를 설정합니다.
+#define MMEDIA_ID @"MILLENNIALMEDIA_ID"
+#define MMEDIA_INTERSTITIAL_ID @"MILLENNIALMEDIA_INTERSTITIAL_ID"
+
+#define MILLENNIAL_IPHONE_AD_VIEW_FRAME CGRectMake(0, 0, 320, 50)
+#define MILLENNIAL_IPAD_AD_VIEW_FRAME CGRectMake(0, 0, 728, 90)
+#define MILLENNIAL_AD_VIEW_FRAME ((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) ? MILLENNIAL_IPAD_AD_VIEW_FRAME : MILLENNIAL_IPHONE_AD_VIEW_FRAME)
+
+@implementation SubAdlibAdViewMMedia
+
+- (void)query:(UIViewController*)parent
+{
+    [super query:parent];
+    
+    // Returns an autoreleased MMAdView object
+    ad = [[MMAdView alloc] initWithFrame:MILLENNIAL_AD_VIEW_FRAME
+                                    apid:MMEDIA_ID
+                      rootViewController:parent];
+    
+    // Ad banner to the view
+    [self.view addSubview:ad];
+    
+    [self queryAd];
+    
+    [self getAd];
+}
+
+- (CGSize)size
+{
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    CGFloat screenWidth = screenRect.size.width;
+    CGFloat screenHeight = screenRect.size.height;
+    
+    int w;
+    if([self isPortrait])
+    {
+        w = screenWidth;
+    }
+    else
+    {
+        w = screenHeight;
+    }
+    
+    if(iPad)    
+        return CGSizeMake(w, 90);
+    else
+        return CGSizeMake(w, 50);
+}
+
+- (void)clearAdView
+{
+    [super clearAdView];
+    
+    if(ad != nil)
+    {
+        ad.rootViewController = nil;
+        ad = nil;
+    }
+}
+
+#pragma mark - Get Ad
+
+- (void)getAd
+{
+    MMRequest *request = [MMRequest request];
+    
+    // Get a banner ad
+    [ad getAdWithRequest:request onCompletion:^(BOOL success, NSError *error) {
+        if (success) {
+            // 화면에 광고를 보여줍니다.
+            [self gotAd];
+        }
+        else {
+            // 광고 수신에 실패하였습니다.
+            [self failed];
+        }
+    }];
+}
+
++ (void)loadInterstitail:(UIViewController*)viewController
+{
+    //MMRequest Object
+    MMRequest *request = [MMRequest request];
+    
+    //Replace YOUR_APID with the APID provided to you by Millennial Media
+    [MMInterstitial fetchWithRequest:request
+                                apid:MMEDIA_INTERSTITIAL_ID
+                        onCompletion:^(BOOL success, NSError *error) {
+                            if (success) {
+                                [MMInterstitial displayForApid:MMEDIA_INTERSTITIAL_ID
+                                            fromViewController:viewController
+                                               withOrientation:MMOverlayOrientationTypeAll
+                                                  onCompletion:^(BOOL success, NSError *error) {
+                                                      if (success) {
+                                                          // Interstitial displayed successfully.
+                                                          [self didReceiveInterstitialAd];
+                                                      }
+                                                      else {
+                                                          // Error displaying interstitial
+                                                          [self didFailToReceiveInterstitialAd];
+                                                      }
+                                                  }];
+                            }else{
+                                // Error fetching ad
+                                [self didFailToReceiveInterstitialAd];
+                            }
+                        }];
+}
+
++ (void)didReceiveInterstitialAd
+{
+    // 전면광고 성공을 알린다.
+    [self interstitialReceived:@"mmedia"];
+}
+
++ (void)didFailToReceiveInterstitialAd
+{
+    // 전면광고 실패를 알린다.
+    [self interstitialFailed:@"mmedia"];
+}
+
+@end
