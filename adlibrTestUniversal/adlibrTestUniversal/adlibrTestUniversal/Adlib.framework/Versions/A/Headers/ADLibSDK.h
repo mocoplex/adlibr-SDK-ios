5c6fd425de91ac98e07e55285aa5bc018225cdd8
gskang <gskang@mocoplex.com>
2015-05-13 11:01:25 +0900

    SDK 4.0.1 update
    - minor bug fix
    - added debug message

--- adlibrTestUniversal/adlibrTestUniversal/adlibrTestUniversal/Adlib.framework/Versions/A/Headers/ADLibSDK.h
+++ adlibrTestUniversal/adlibrTestUniversal/adlibrTestUniversal/Adlib.framework/Versions/A/Headers/ADLibSDK.h
@@ -5,8 +5,8 @@
 //  Created by mocoplex on 2014. 12. 30..
 //  Copyright (c) 2014년 mocoplex. All rights reserved.
 //
-//  Adlib SDK version 4.0.0
-//  Build. 2015.03.19. 14:00
+//  Adlib SDK version 4.0.1
+//  Build. 2015.05.19. 17:00
 
 //Core Object
 #import <Adlib/Adlib.h>
