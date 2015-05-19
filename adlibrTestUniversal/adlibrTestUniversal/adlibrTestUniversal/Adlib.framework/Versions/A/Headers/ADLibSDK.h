1874f0a15abc1bd83217ed170775996201963111
gskang <gskang@mocoplex.com>
2015-05-19 11:09:04 +0900

    SDK 4.0.2 update
    - Performance Improvement for mediation-scheduler

--- adlibrTestUniversal/adlibrTestUniversal/adlibrTestUniversal/Adlib.framework/Versions/A/Headers/ADLibSDK.h
+++ adlibrTestUniversal/adlibrTestUniversal/adlibrTestUniversal/Adlib.framework/Versions/A/Headers/ADLibSDK.h
@@ -5,8 +5,8 @@
 //  Created by mocoplex on 2014. 12. 30..
 //  Copyright (c) 2014년 mocoplex. All rights reserved.
 //
-//  Adlib SDK version 4.0.1
-//  Build. 2015.05.19. 17:00
+//  Adlib SDK version 4.0.2
+//  Build (4.002) 2015.05.19. 18:00
 
 //Core Object
 #import <Adlib/Adlib.h>
--- adlibrTestUniversal/adlibrTestUniversal/adlibrTestUniversal/Adlib.framework/Versions/A/Headers/Adlib.h
+++ adlibrTestUniversal/adlibrTestUniversal/adlibrTestUniversal/Adlib.framework/Versions/A/Headers/Adlib.h
@@ -5,8 +5,8 @@
 //  Created by Mocoplex on 2015. 03. 18..
 //  Copyright (c) 2015년 mocoplex. All rights reserved.
 //
-//  Adlib SDK version 4.0.1
-//  Build. 2015.05.19. 17:00
+//  Adlib SDK version 4.0.2
+//  Build (4.002) 2015.05.19. 18:00
 
 #import <Adlib/AdlibManager.h>
 #import <Adlib/SubAdlibAdsView.h>
