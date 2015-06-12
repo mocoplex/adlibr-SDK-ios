63328f9c3dc3fc2e0be5610fad78ebfefd5fd14c
gskang <gskang@mocoplex.com>
2015-06-12 15:12:11 +0900

    SDK v.4.1.0.0 update
    - Support 3D ads

--- adlibrTestUniversal/adlibrTestUniversal/adlibrTestUniversal/Adlib.framework/Versions/A/Headers/Adlib.h
+++ adlibrTestUniversal/adlibrTestUniversal/adlibrTestUniversal/Adlib.framework/Versions/A/Headers/Adlib.h
@@ -5,8 +5,8 @@
 //  Created by Mocoplex on 2015. 03. 18..
 //  Copyright (c) 2015년 mocoplex. All rights reserved.
 //
-//  Adlib SDK version 4.0.2
-//  Build (4.002) 2015.05.19. 18:00
+//  Adlib SDK version 4.1.0.0
+//  Build (4.100) 2015.06.12. 12:00
 
 #import <Adlib/AdlibManager.h>
 #import <Adlib/SubAdlibAdsView.h>
