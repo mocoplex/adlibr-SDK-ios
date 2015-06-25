94d7ea1a33abc2733dd73f0acb58971de2c015e8
gskang <gskang@mocoplex.com>
2015-06-25 16:36:01 +0900

    SDK Update v.4.1.1.0
    - Update Adlib.framework v.4.1.1.0

--- adlibrTestUniversal/adlibrTestUniversal/adlibrTestUniversal/Adlib.framework/Versions/A/Headers/ADLibSDK.h
+++ adlibrTestUniversal/adlibrTestUniversal/adlibrTestUniversal/Adlib.framework/Versions/A/Headers/ADLibSDK.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014년 mocoplex. All rights reserved.
 //
 //  Adlib SDK version 4.1.1.0
-//  Build (4.110) 2015.06.25. 16:00
+//  Build (4.110) 2015.06.25. 16:30
 
 //Core Object
 #import <Adlib/Adlib.h>
