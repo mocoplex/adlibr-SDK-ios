b633ecdf55b9d2dcfac70882b71c8bba6fb5e4b5
gskang <gskang@mocoplex.com>
2015-07-06 18:10:18 +0900

    Update SDK v.4.1.2.0
    - minor fix for webView

--- AdlibUnityPlugin_iOS_1.3/ADLIB/Adlib.framework/Versions/A/Headers/Adlib.h
+++ AdlibUnityPlugin_iOS_1.3/ADLIB/Adlib.framework/Versions/A/Headers/Adlib.h
@@ -5,8 +5,8 @@
 //  Created by Mocoplex on 2015. 03. 18..
 //  Copyright (c) 2015년 mocoplex. All rights reserved.
 //
-//  Adlib SDK version 4.1.1.0
-//  Build (4.110) 2015.06.25. 16:30
+//  Adlib SDK version 4.1.2.0
+//  Build (4.120) 2015.07.06. 16:00
 
 #import <Adlib/AdlibManager.h>
 #import <Adlib/SubAdlibAdsView.h>
