76de304f085c5856fbad2f1e1ece0e03cf6d344f
gskang <gskang@mocoplex.com>
2015-07-31 11:43:35 +0900

    SDK Update v.4.1.3.1
    - Updated adlib SDK

---
+++ HelloAdlib/Adlib.framework/Versions/A/Headers/ALNativeAdRequest.h
@@ -0,0 +1,22 @@
+//
+//  ALNativeAdRequest.h
+//  AdlibNativeADSample
+//
+//  Created by mocoplex on 2014. 12. 9..
+//  Copyright (c) 2014년 mocoplex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, ALAdRequestItemType){
+    
+    ALAdRequestItemTypeImageAd = 1,
+    ALAdRequestItemTypeVideoAd = 2,
+    ALAdRequestItemTypeAll     = 3,
+};
+
+@interface ALNativeAdRequest : NSObject {
+    
+}
+
+@end
--- HelloAdlib/Adlib.framework/Versions/A/Headers/Adlib.h
+++ HelloAdlib/Adlib.framework/Versions/A/Headers/Adlib.h
@@ -5,8 +5,8 @@
 //  Created by Mocoplex on 2015. 03. 18..
 //  Copyright (c) 2015년 mocoplex. All rights reserved.
 //
-//  Adlib SDK version 4.1.2.0
-//  Build (4.120) 2015.07.06. 16:00
+//  Adlib SDK version 4.1.3.1
+//  Build (4.130) 2015.07.31. 11:00
 
 #import <Adlib/AdlibManager.h>
 #import <Adlib/SubAdlibAdsView.h>
