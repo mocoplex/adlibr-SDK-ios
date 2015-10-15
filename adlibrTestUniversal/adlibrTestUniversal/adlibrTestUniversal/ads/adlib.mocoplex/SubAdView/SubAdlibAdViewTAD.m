544c7dddc99c5f0c5bf862a3e7f928679592ef61
gskang <gskang@mocoplex.com>
2015-10-15 16:25:47 +0900

    update SubAdlibAdViewTAD
    update SubAdlibAdViewTAD.m
    - support TAD interstitialAD

--- adlibrTestUniversal/adlibrTestUniversal/adlibrTestUniversal/ads/adlib.mocoplex/SubAdView/SubAdlibAdViewTAD.m
+++ adlibrTestUniversal/adlibrTestUniversal/adlibrTestUniversal/ads/adlib.mocoplex/SubAdView/SubAdlibAdViewTAD.m
@@ -21,6 +21,8 @@
 
 @interface SubAdlibAdViewTAD () <TadDelegate>
 
+@property (nonatomic, strong) TadCore *interstitial;
+
 @end
 
 @implementation SubAdlibAdViewTAD
@@ -34,28 +36,32 @@
 {
     [super query:parent];
     
-    TadCore *ad = [[TadCore alloc] initWithSeedView:self.view delegate:self];
-    self.tadCore = ad;
+    if (self.tadCore == nil) {
+        TadCore *ad = [[TadCore alloc] initWithSeedView:self.view delegate:self];
+        self.tadCore = ad;
+      
+        //필수 사항
+        // 테스트할 Client ID (필수 사항)
+        [ad setClientID:TAD_ID];
+        // 테스트할 슬롯을 설정해준다. (필수 사항)
+        [ad setSlotNo:TadSlotInline];
+        
+        // 상용서버를 사용 할지 테스트서버를 사용 할지 결정해준다.
+        [ad setIsTest:YES];
+        
+        [ad setSeedViewController:parent]; // 광고가 표시되는 ViewController를 전달
+        
+        // 선택 셋팅 사항
+        //CGPoint offset = [self getTadOffSet];
+        //[ad setOffset:offset];  // 광고의 오프셋을 결정한다. (Default 0.0)
+        
+        [ad setRefershInterval:20.0f];      // 광고 재요청 시간 간격을 설정한다. 15~60초 (Default : 20)
+        [ad setIsMediation:NO];
+        
+        [ad setLogMode:NO];               // 로그를 보여줄지 아닐지 결정 (Default : NO)
+        [ad setUseBackFillColor:YES];
+    }
     
-    //필수 사항
-    // 테스트할 Client ID (필수 사항)
-    [ad setClientID:TAD_ID];
-    // 테스트할 슬롯을 설정해준다. (필수 사항)
-    [ad setSlotNo:TadSlotInline];
-    
-    // 상용서버를 사용 할지 테스트서버를 사용 할지 결정해준다.
-    [ad setIsTest:YES];
-    
-    [ad setSeedViewController:parent]; // 광고가 표시되는 ViewController를 전달
-    
-    // 선택 셋팅 사항
-    [ad setOffset:CGPointMake(0, 0)];  // 광고의 오프셋을 결정한다. (Default 0.0)
-    [ad setRefershInterval:0.0f];      // 광고 재요청 시간 간격을 설정한다. 15~60초 (Default : 20)
-    [ad setIsMediation:NO];
-    
-    [ad setLogMode:NO];               // 로그를 보여줄지 아닐지 결정 (Default : NO)
-    [ad setUseBackFillColor:YES];
-
     [self queryAd];
     
     [_tadCore viewWillAppear:NO];
@@ -75,24 +81,28 @@
     }
 }
 
-- (CGSize)size
-{
-    CGFloat w = self.view.bounds.size.width;
-    return CGSizeMake(w, 50);
-}
-
 - (void)orientationChanged
 {
-    [_tadCore setOffset:CGPointMake(0, 0)];
+    [super orientationChanged];
+    
+    //[_tadCore setOffset:CGPointMake(0, 0)];
 }
 
-- (void)loadInterstitail:(UIViewController*)viewController
+- (void)subAdlibViewLoadInterstitial:(UIViewController*)viewController
 {
-    TadCore *interstitial = [[TadCore alloc] initWithSeedView:viewController.view delegate:self];
+    if (self.interstitial) {
+        [self.interstitial destroyAd];
+        self.interstitial.delegate = nil;
+        self.interstitial = nil;
+    }
+    
+    TadCore *interstitial = [[TadCore alloc] initWithSeedView:self.view delegate:self];
+    self.interstitial = interstitial;
     
     //필수 사항
     [interstitial setClientID:TAD_INTERSTITIAL_ID];             // 클라이언트 아이디
     [interstitial setSlotNo:TadSlotInterstitial];               // 슬롯 설정
+    [interstitial setSeedViewController:viewController];
     
     // 선택 셋팅 사항
     [interstitial setIsTest:YES];                               // YES : 테스트 서버, NO : 상용 서버 (Default : YES)
@@ -106,44 +116,73 @@
 - (CGPoint)getTadOffSet
 {
     CGPoint pt = CGPointZero;
-//    CGFloat originX = (self.view.bounds.size.width - kTAD_BANNER_SIZE.width)/2;
-//    CGFloat originY = self.view.bounds.size.height - kTAD_BANNER_SIZE.height;
-//    if (originX < 0) {
-//        originX = 0;
-//    }
-//    if (originY < 0) {
-//        originY = 0;
-//    }
-//    pt = CGPointMake(originX, originY);
-    
-    pt = CGPointMake(0, 0);
+    CGFloat originX = (self.view.bounds.size.width - kTAD_BANNER_SIZE.width)/2;
+    CGFloat originY = self.view.bounds.size.height - kTAD_BANNER_SIZE.height;
+    if (originX < 0) {
+        originX = 0;
+    }
+    if (originY < 0) {
+        originY = 0;
+    }
+    pt = CGPointMake(originX, originY);
     
     return pt;
 }
 
 #pragma mark - TadDelegate
 
-- (void)tadOnAdLoaded:(TadCore *)tadCore {
-    //광고 로드 완료
-    [self gotAd];
-    bGotAd = YES;
+- (void)tadOnAdLoaded:(TadCore *)tadCore
+{
+    if (tadCore == self.interstitial) {
+        
+        // 전면광고 성공을 알린다.
+        [self subAdlibViewInterstitialReceived:@"tad"];
+        
+        UIViewController *ctr = self.interstitial.interstialViewController;
+        if (ctr && self.interstitial.canLoadInterstitial) {
+            [self.parentController presentViewController:ctr
+                                                animated:YES
+                                              completion:nil];
+        }
+    } else if (tadCore == self.tadCore) {
+       
+        //광고 로드 완료
+        [self gotAd];
+        bGotAd = YES;
+    }
 }
 
 - (void)tadCore:(TadCore *)tadCore tadOnAdFailed:(TadErrorCode)errorCode {
     
     // 실패했다. 바로 다음 스케줄 광고를 보인다.
-    NSLog(@"tadCore adFailed:%zd", errorCode);
     
-    [self failed];
-    bGotAd = NO;
+    if (tadCore == self.interstitial) {
+        NSLog(@"tadCore interstitial adFailed:%zd", errorCode);
+        
+        // 전면광고 실패를 알린다.
+        [self subAdlibViewInterstitialFailed:@"tad"];
+        
+    } else if (tadCore == self.tadCore) {
+        NSLog(@"tadCore band adFailed:%zd", errorCode);
+        
+        [self failed];
+        bGotAd = NO;
+    }
 }
 
 - (void)tadOnAdClicked:(TadCore *)tadCore {
     
     //NSLog(@"<Tad> 광고 클릭");
-    
     //광고 클릭 이벤트를 리포팅
-    [self reportBannerClickEvent];
+    
+    if (tadCore == self.interstitial) {
+        
+        [self reportInterstitialClickEvent];
+        
+    } else if (tadCore == self.tadCore) {
+        
+        [self reportBannerClickEvent];
+    }
 }
 
 @end
